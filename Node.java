885131b075c13eee2fe793f9e4af54669fd9fcd3
lamdaham <ilam21@stuy.edu>
2021-01-05 14:25:54 -0500

    get/set methods

--- Node.java
+++ Node.java
@@ -7,4 +7,25 @@ public class Node{
 		this.prev = null;
 	}
 	//write get/set methods for all three instance variables.
+	public static void setNext(Node index) {
+		this.next = index;
+	}
+	public static void setPrev(Node index) {
+		this.prev = index;
+	}
+	public static void setNext(String element) {
+		this.data = element;
+	}
+
+
+	public static Node getNext() {
+		return this.next;
+	}
+	public static Node getPrev() {
+		return this.prev;
+	}
+	public static String getData() {
+		return this.data;
+	}
+
 }
